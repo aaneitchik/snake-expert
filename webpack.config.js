d22b74b1220e8ce2170f3093f7acf01505bf6543
Ann Aneychik <aaneitchik@gmail.com>
2017-07-12 12:50:56 +0300

    Add hashes to .js built files

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = {
 	output: {
 		path: path.join(__dirname, 'dist'),
 		publicPath: '',
-		filename: '[name].js'
+		filename: '[name].[hash].js'
 	},
 	devtool: PRODUCTION ? false : 'source-map',
 	plugins,
